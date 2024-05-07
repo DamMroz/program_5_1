c5e232849f75bd5e192ab693d7a1dc7c78a37965
Damian Mroz <damianmroz141@gmail.com>
2024-05-07 15:01:44 +0200

    Modify task_5_1.py

--- task_5_1.py
+++ task_5_1.py
@@ -3,20 +3,30 @@ fake = Faker()
 
 
 class Person:
-    def __init__(self, first_name, last_name,phone, mail,*args,**kwargs):
-           
+    def __init__(self, first_name, last_name,phone, mail):
         self.first_name = first_name
         self.last_name = last_name
         self.phone = phone
         self.mail= mail
-    
+
+
     @property
     def label_length(self):
-        self.length=len(self.first_name)+len(self.last_name)    
-        return self.length 
-        
+        return len(f"{self.first_name} {self.last_name}")    
+
+
+    @property
+    def contact_phone(self):
+        return self.phone
+    
+
     def contact(self):
-        return f"Wybieram numer {self.phone} i dzwonię do {self.first_name} {self.last_name}" 
+        return (f"Wybieram numer {self.contact_phone} i dzwonię do "
+        f"{self.first_name} {self.last_name} {self.label_length}") 
+
+
+    def __repr__(self):
+        print( f"{self.first_name} {self.last_name}")
 
 
 class BaseContact(Person):
@@ -24,54 +34,77 @@ class BaseContact(Person):
 
 
 class BusinessContact(Person):
-    def __init__(self,company,work_phone,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self, first_name, last_name,phone, mail,company, work_phone):
+        super().__init__(first_name, last_name,phone, mail)
         self.company=company
         self.work_phone=work_phone
 
-    def business_contact(self):
-        return f"Wybieram numer {self.work_phone} i dzwonię do {self.first_name} {self.last_name}"
+
+    @property
+    def contact_phone(self):
+        return self.work_phone
+
+
+def create_contacts(type, amount):
+    contact_list=[]
+    for i in range(amount):
+        first_name = fake.first_name()
+
+        last_name=fake.last_name()
+
+        phone=f"0048{fake.msisdn()}"
+
+        work_phone=fake.msisdn()
+
+        mail=f"{first_name}.{last_name}@{fake.domain_name()}" 
+
+        company=fake.company()
+
+        work_phone=fake.msisdn()
+
+        if type == "base":
+
+            contact = BaseContact(
+
+            first_name=first_name,
+
+            last_name=last_name,
+
+            phone=phone,
+
+            mail=mail,
+            ).contact()
+
+        elif type == "business":
+
+            contact = BusinessContact(
+
+            first_name=first_name,
+
+            last_name=last_name,
+
+            phone=phone,
+
+            mail=mail,
+
+            company=company,
+
+            work_phone=work_phone
+
+            ).contact()
+
+        contact_list.append(contact)
+
+
+    return contact_list
 
 
 if __name__ == "__main__":
-    type=input("Please provide type: ") 
-    additional=input("Do do want random information?: ")
-    if additional=="Yes":
-        amount=int(input("Please provide amount: ")) 
-    elif additional=="No":
-        amount=0
+    type=input("Please provide the type of contact: ")
+    amount=input("Please provide the desired amount: ")
 
+    
+    print(create_contacts(type, int(amount)))
 
-    def create_contacts(type,amount):
-        contact_list=[]
-        print("\nGenerated output: ")
-        if type=="Base_contact":
-            for x in range (0,amount):
-                first_name=fake.first_name()
-                last_name=fake.last_name()
-                phone=f"0048{fake.msisdn()}"
-                mail=f"{first_name}.{last_name}@{fake.domain_name()}" 
+    
 
-                contact_list.append((first_name,last_name,phone, mail))
-            for x in range (0,amount): 
-                contact=BaseContact(*contact_list[x]).contact()
-                contact_length=BaseContact(*contact_list[x]).label_length
-                print(contact,contact_length)
-        elif type=="Business_contact":
-            for x in range (0,amount):
-                company=fake.company()
-                work_phone=fake.msisdn()
-                first_name=fake.first_name()
-                last_name=fake.last_name()
-                phone=fake.msisdn()
-                position=fake.job()
-                mail=f"{first_name}.{last_name}@{fake.domain_name()}" 
-            
-                contact_list.append((company,work_phone,first_name,last_name,phone, position,mail))
-            for x in range (0,amount):    
-                contact=BusinessContact(*contact_list[x]).business_contact()
-                contact_length=BusinessContact(*contact_list[x]).label_length    
-                print(contact,contact_length)
-
-
-    create_contacts(type,amount)
